f1e6ff2035cf9652b0993cfbf3ad9bdfedd35e43
Luigi Marino <luigimarinodev@gmail.com>
2023-05-22 15:53:39 +0200

    Refactoring es 4

--- 4/main.c
+++ 4/main.c
@@ -23,15 +23,24 @@ int main(){
     pthread_t t1,t2,t3,t4;
     pthread_mutex_init(&mutex, NULL);
 
-    if (pthread_create(&t1, NULL, &routine, NULL)!=0) return 1; 
-    if (pthread_create(&t2, NULL, &routine, NULL)!=0) return 2;
-    if(pthread_create(&t3, NULL, &routine, NULL)!=0) return 3;
-    if(pthread_create(&t4, NULL, &routine, NULL)!=0) return 4;
+    if (pthread_create(&t1, NULL, &routine, NULL)!=0) 
+        return 1; 
 
-    if(pthread_join(t1,NULL)!= 0 ) return 5;
-    if(pthread_join(t2,NULL)!= 0 ) return 5;
-    if(pthread_join(t3,NULL)!= 0 ) return 5;
-    if(pthread_join(t4,NULL)!= 0 ) return 5;
+    if (pthread_create(&t2, NULL, &routine, NULL)!=0) 
+        return 2;
+    if(pthread_create(&t3, NULL, &routine, NULL)!=0) 
+        return 3;
+    if(pthread_create(&t4, NULL, &routine, NULL)!=0) 
+        return 4;
+
+    if(pthread_join(t1,NULL)!= 0 ) 
+        return 5;
+    if(pthread_join(t2,NULL)!= 0 ) 
+        return 6;
+    if(pthread_join(t3,NULL)!= 0 ) 
+        return 7;
+    if(pthread_join(t4,NULL)!= 0 ) 
+        return 8;
 
     printf("mails: %d\n", mails);
 
