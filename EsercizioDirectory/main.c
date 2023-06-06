d5d6b7e46d0aa06c3c3e710149143c5e5b89eda5
Luigi Marino <luigimarinodev@gmail.com>
2023-06-06 11:14:49 +0200

    Update main.c

--- EsercizioDirectory/main.c
+++ EsercizioDirectory/main.c
@@ -1,3 +1,4 @@
-/Si realizzi un programma in C e Posix sotto Linux che, con  l'ausilio della
-libreria Pthread, lancia 5 thread che leggono concorrentemente altrettante directory specificate dall'utente.   I thread, inoltre, scrivono in una variabile il numero di  file della directory
-che,  tra  le tre esaminate, contiene il  minor numero di entrate./
+/*Si realizzi un programma in C e Posix sotto Linux che, con  l'ausilio della
+libreria Pthread, lancia 5 thread che leggono concorrentemente altrettante directory specificate dall'utente.   
+I thread, inoltre, scrivono in una variabile il numero di  file della directory
+che,  tra  le tre esaminate, contiene il  minor numero di entrate.*/
